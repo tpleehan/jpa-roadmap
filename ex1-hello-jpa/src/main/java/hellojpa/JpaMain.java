c676b6876065fde138020a6dde530186945b301f
tpleehan <tpleehan@gmail.com>
2023-02-18 22:08:13 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 - 영속성 관리-내부 동작 방식(플러시)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,15 +18,16 @@ public class JpaMain {
 
 		try {
 
-			Member member = em.find(Member.class, 150L);
-			member.setName("ZZZZZ");
+			Member member = new Member(200L, "member200");
+			em.persist(member);
 
-			// entity의 값만 변경하면 무조건 update query가 실행되고, 트랜잭션 커밋되는 시점에 변경을 반영한다.
-			// 값을 변경하는 경우에는 persist 또는 update와 같이 호출하지 않는다.
-			// em.persist(member);
+			// 플러시는 영속성 컨텍스트를 비우지 않는다.
+			// 영속성 컨텍스트의 변경 내용을 데이터베이스에 동기화한다.
+			// 트랜잭션이라는 작업 단위가 중요하며, 커밋 직전에만 동기화 하면 된다.
+			// em.setFlushMode(FlushModeType.COMMIT, AUTO) 모드 옵션을 바꿀 수 있지만 Auto 기본값 사용하는 것을 권장
+			em.flush();
 
 			System.out.println("===============");
-
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
