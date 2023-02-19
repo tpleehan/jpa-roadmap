d11a961f59a49e102d905fe6d6ba4070e2a35eb9
tpleehan <tpleehan@gmail.com>
2023-02-19 22:38:15 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 - 엔티티 매핑(필드와 컬럼 매핑)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,13 @@ public class JpaMain {
 		tx.begin();
 
 		try {
+			Member member = new Member();
+			member.setId(3L);
+			member.setUsername("C");
+			member.setRoleType(RoleType.GUEST);
 
-			Member member = em.find(Member.class, 150L);
-			member.setName("AAAAA");
+			em.persist(member);
 
-			em.clear();
-
-			Member member2 = em.find(Member.class, 150L);
-
-			System.out.println("===============");
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,8 +1,18 @@
 package hellojpa;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 public class Member {
@@ -10,15 +20,33 @@ public class Member {
 	@Id
 	private Long id;
 
-	@Column(unique = true, length = 10)
-	private String name;
+	// @Column에 name 속성을 지정하면 데이터베이스에서 사용되는 name이 된다.
+	@Column(name = "name", nullable = false)
+	private String username;
+
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	// @Temporal에 TemporalType은 DATE, TIME, TIMESTAMP를 통해 데이터베이스에 날짜와 관련된 부분을 적절하게 지정할 수 있다.
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	private LocalDate testLocalDate;
+	private LocalDateTime testLocalDateTime;
+
+	@Lob
+	private String description;
+
+	// @Transient 데이터베이스가 아닌 메모리에 임시로 값을 보관하거나 사용할 때 선언한다.
+	@Transient
+	private int temp;
 
 	public Member() {
-	}
-
-	public Member(final Long id, final String name) {
-		this.id = id;
-		this.name = name;
 	}
 
 	public Long getId() {
@@ -29,11 +57,59 @@ public class Member {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setName(final String name) {
-		this.name = name;
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(final Integer age) {
+		this.age = age;
+	}
+
+	public RoleType getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(final RoleType roleType) {
+		this.roleType = roleType;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(final Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(final Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	public int getTemp() {
+		return temp;
+	}
+
+	public void setTemp(final int temp) {
+		this.temp = temp;
 	}
 }
