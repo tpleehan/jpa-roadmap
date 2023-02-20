d1b23ae3d5ee514e87aba1d361be4d419dc7a7b5
tpleehan <tpleehan@gmail.com>
2023-02-20 11:18:55 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 - 연관관계 매핑 기초(단방향 연관관계)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,25 +17,27 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			Member member1 = new Member();
-			member1.setUsername("A");
 
-			Member member2 = new Member();
-			member2.setUsername("B");
+			// 저장
+			Team team = new Team();
+			team.setName("TeamA");
+			em.persist(team);
 
-			Member member3 = new Member();
-			member3.setUsername("C");
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setTeam(team);
 
-			System.out.println("=========");
+			em.persist(member);
 
-			em.persist(member1); // DB 1, 51
-			em.persist(member2); // MEM
-			em.persist(member3); // MEM
+			em.flush();
+			em.clear();
 
-			System.out.println("member1.id = " + member1.getId());
-			System.out.println("member2.id = " + member2.getId());
-			System.out.println("member3.id = " + member3.getId());
-			System.out.println("=========");
+			Member findMember = em.find(Member.class, member.getId());
+			Team findTeam = findMember.getTeam();
+			System.out.println("findTeam = " + findTeam.getName());
+
+			Team newTeam = em.find(Team.class, 100L);
+			findMember.setTeam(newTeam);
 
 			tx.commit();
 		} catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,33 +3,27 @@ package hellojpa;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
-@SequenceGenerator(
-	name = "MEMBER_SEQ_GENERATOR",
-	sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
-	initialValue = 1, allocationSize = 50
-)
 public class Member {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,
-		generator = "MEMBER_SEQ_GENERATOR")
+	@GeneratedValue
+	@Column(name = "MEMBER_ID")
 	private Long id;
 
-	@Column(name = "name", nullable = false)
+	@Column(name = "USERNAME")
 	private String username;
 
-	public Member() {
-	}
+	// @Column(name = "TEAM_ID")
+	// private Long teamId;
 
-	public Member(Long id, String username) {
-		this.id = id;
-		this.username = username;
-	}
+	@ManyToOne
+	@JoinColumn(name = "TEAM_ID")
+	private Team team;
 
 	public Long getId() {
 		return id;
@@ -45,5 +39,13 @@ public class Member {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
 	}
 }
