9bd4209e27d43fc068590e4e70679cb911f6ef7c
tpleehan <tpleehan@gmail.com>
2023-02-23 20:18:32 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 - 객체지향 쿼리 언어1 - 기본 문법(기본 문법과 쿼리 API)

---
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,39 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+
+		tx.begin();
+
+		try {
+
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setAge(10);
+			em.persist(member);
+
+			Member result = em.createQuery("SELECT m from Member m where m.username = :username",
+				Member.class).setParameter("username", "member1").getSingleResult();
+
+			System.out.println("singleResult = " + result.getUsername());
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		} finally {
+			em.close();
+		}
+		emf.close();
+	}
+}
---
+++ jpql/src/main/java/jpql/Order.java
@@ -0,0 +1,42 @@
+package jpql;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private int orderAmount;
+	@Embedded
+	private Address address;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "PRODUCT_ID")
+	private Product product;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getOrderAmount() {
+		return orderAmount;
+	}
+
+	public void setOrderAmount(int orderAmount) {
+		this.orderAmount = orderAmount;
+	}
+}
