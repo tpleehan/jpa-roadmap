90008d2166165e177989e5d792076463d8f76b14
tpleehan <tpleehan@gmail.com>
2023-02-23 17:17:57 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 - 값 타입(값 타입 컬렉션)

--- ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
@@ -25,7 +27,7 @@ public class Address {
 		return city;
 	}
 
-	public void setCity(String city) {
+	private void setCity(String city) {
 		this.city = city;
 	}
 
@@ -33,8 +35,31 @@ public class Address {
 		return street;
 	}
 
+	private void setStreet(String street) {
+		this.street = street;
+	}
+
 	public String getZipcode() {
 		return zipcode;
 	}
 
+	private void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Address address = (Address)o;
+		return Objects.equals(city, address.city) && Objects.equals(street, address.street)
+			&& Objects.equals(zipcode, address.zipcode);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, street, zipcode);
+	}
 }
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,10 +1,20 @@
 package hellojpa;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Member {
@@ -17,13 +27,21 @@ public class Member {
 	@Column(name = "USERNAME")
 	private String username;
 
-	// 기간 Period
-	@Embedded
-	private Period workPeriod;
-
-	// 주소 address
 	@Embedded
 	private Address homeAddress;
+
+	@ElementCollection
+	@CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+	@Column(name = "FOOD_NAME")
+	private Set<String> favoriteFoods = new HashSet<>();
+
+	// @ElementCollection
+	// @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+	// private List<Address> addressHistory = new ArrayList<>();
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "MEMBER_ID")
+	private List<AddressEntity> addressHistory = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -41,19 +59,27 @@ public class Member {
 		this.username = username;
 	}
 
-	public Period getWorkPeriod() {
-		return workPeriod;
-	}
-
-	public void setWorkPeriod(Period workPeriod) {
-		this.workPeriod = workPeriod;
-	}
-
 	public Address getHomeAddress() {
 		return homeAddress;
 	}
 
 	public void setHomeAddress(Address homeAddress) {
 		this.homeAddress = homeAddress;
+	}
+
+	public Set<String> getFavoriteFoods() {
+		return favoriteFoods;
+	}
+
+	public void setFavoriteFoods(Set<String> favoriteFoods) {
+		this.favoriteFoods = favoriteFoods;
+	}
+
+	public List<AddressEntity> getAddressHistory() {
+		return addressHistory;
+	}
+
+	public void setAddressHistory(List<AddressEntity> addressHistory) {
+		this.addressHistory = addressHistory;
 	}
 }
