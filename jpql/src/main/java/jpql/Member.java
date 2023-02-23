9452e1130ac0c5162ec20ad2a33d443bb053e6fa
tpleehan <tpleehan@gmail.com>
2023-02-23 23:55:01 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 - 객체지향 쿼리 언어1 - 기본 문법(페이징)

--- jpql/src/main/java/jpql/Member.java
+++ jpql/src/main/java/jpql/Member.java
@@ -43,4 +43,13 @@ public class Member {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "Member{" +
+			"id=" + id +
+			", username='" + username + '\'' +
+			", age=" + age +
+			'}';
+	}
 }
