4d822881183aedcd59bf8b52baed60b6f946565b
tpleehan <tpleehan@gmail.com>
2023-02-23 23:33:58 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 - 객체지향 쿼리 언어1 - 기본 문법(프로젝션(SELECT))

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,5 +1,7 @@
 package jpql;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -22,10 +24,16 @@ public class JpaMain {
 			member.setAge(10);
 			em.persist(member);
 
-			Member result = em.createQuery("SELECT m from Member m where m.username = :username",
-				Member.class).setParameter("username", "member1").getSingleResult();
+			em.flush();
+			em.clear();
 
-			System.out.println("singleResult = " + result.getUsername());
+			List<MemberDTO> result = em.createQuery(
+					"SELECT new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+				.getResultList();
+
+			MemberDTO memberDTO = result.get(0);
+			System.out.println("memberDTO = " + memberDTO.getUsername());
+			System.out.println("memberDTO = " + memberDTO.getAge());
 
 			tx.commit();
 		} catch (Exception e) {
