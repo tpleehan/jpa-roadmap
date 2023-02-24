661f11fccf411babb1c69fb59c2e84b824164455
tpleehan <tpleehan@gmail.com>
2023-02-24 22:55:02 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 - 객체지향 쿼리 언어1 - 기본 문법(조인)

--- jpql/src/main/java/jpql/Member.java
+++ jpql/src/main/java/jpql/Member.java
@@ -20,6 +20,11 @@ public class Member {
 	@JoinColumn(name = "TEAM_ID")
 	private Team team;
 
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -42,6 +47,14 @@ public class Member {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
 	}
 
 	@Override
