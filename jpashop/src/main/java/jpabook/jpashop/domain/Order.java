3bcec9757a57c1c8ca9579719664ee3cd71fc06d
tpleehan <tpleehan@gmail.com>
2023-02-28 10:24:04 +0900

    실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 - 도메인 분석 설계(엔티티 클래스 개발2)

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,7 +8,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -31,7 +30,7 @@ public class Order {
 	@Column(name = "order_id")
 	private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne
 	@JoinColumn(name = "member_id")
 	private Member member;
 
--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -24,7 +24,7 @@ public class MemberRepositoryTest {
 	public void testMember() throws Exception {
 		// given
 		Member member = new Member();
-		member.setUsername("memberA");
+		member.setName("memberA");
 
 		// when
 		Long savedId = memberRepository.save(member);
@@ -32,7 +32,7 @@ public class MemberRepositoryTest {
 
 		// then
 		Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-		Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+		Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
 		Assertions.assertThat(findMember).isEqualTo(member);
 		System.out.println("findMember == member = " + (findMember == member));
 
