f8611d273720533bd959578b71baf620eec590ce
tpleehan <tpleehan@gmail.com>
2023-03-02 00:27:22 +0900

    실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 - 웹 계층 개발(변경 감지와 병합(merge))

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -21,6 +21,14 @@ public class ItemService {
 		itemRepository.save(item);
 	}
 
+	@Transactional
+	public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+		Item findItem = itemRepository.findOne(itemId);
+		findItem.setName(name);
+		findItem.setPrice(price);
+		findItem.setStockQuantity(stockQuantity);
+	}
+
 	public List<Item> findItems() {
 		return itemRepository.findAll();
 	}
@@ -28,5 +36,5 @@ public class ItemService {
 	public Item findOne(Long itemId) {
 		return itemRepository.findOne(itemId);
 	}
-	
+
 }
