a171abd20cf89b7c915808b05ca9ff4e23fcdcd5
tpleehan <tpleehan@gmail.com>
2023-03-02 15:09:48 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 - API 개발 기본(회원 수정 API)

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,12 +3,15 @@ package jpabook.jpashop.api;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -38,6 +41,28 @@ public class MemberApiController {
 		return new CreateMemberResponse(id);
 	}
 
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(
+		@PathVariable("id") Long id,
+		@RequestBody @Valid UpdateMemberRequest request) {
+
+		memberService.update(id, request.getName());
+		Member findMember = memberService.findOne(id);
+		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+	}
+
+	@Data
+	static class UpdateMemberRequest {
+		private String name;
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
+	}
+
 	@Data
 	static class CreateMemberRequest {
 		@NotEmpty
@@ -52,5 +77,4 @@ public class MemberApiController {
 			this.id = id;
 		}
 	}
-
 }
