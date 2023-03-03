7d83c2b431d3c85ee12e73529d79dd16ff300dc0
tpleehan <tpleehan@gmail.com>
2023-03-04 02:10:30 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 - API 개발 고급 - 컬렉션 조회 최적화(주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화)

--- jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -6,10 +6,12 @@ import java.util.List;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(of = "order_id")
 public class OrderQueryDto {
-	
+
 	private Long orderId;
 	private String name;
 	private LocalDateTime orderDate;
@@ -23,5 +25,14 @@ public class OrderQueryDto {
 		this.orderDate = orderDate;
 		this.orderStatus = orderStatus;
 		this.address = address;
+	}
+
+	public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+		this.orderId = orderId;
+		this.name = name;
+		this.orderDate = orderDate;
+		this.orderStatus = orderStatus;
+		this.address = address;
+		this.orderItems = orderItems;
 	}
 }
