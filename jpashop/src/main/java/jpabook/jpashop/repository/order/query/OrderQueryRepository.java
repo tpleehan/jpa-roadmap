cf5c83d881abec9867bf7d613474fa44390892a7
tpleehan <tpleehan@gmail.com>
2023-03-04 01:45:16 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 - API 개발 고급 - 컬렉션 조회 최적화(주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화)

--- jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.repository.order.query;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 
@@ -21,6 +23,37 @@ public class OrderQueryRepository {
 			o.setOrderItems(orderItems);
 		});
 		return result;
+	}
+
+	public List<OrderQueryDto> findAllByDto_optimization() {
+		List<OrderQueryDto> result = findOrders();
+
+		Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+		result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+		return result;
+	}
+
+	private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+		List<OrderItemQueryDto> orderItems = em.createQuery(
+				"select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)"
+					+ " from OrderItem oi"
+					+ " join oi.item i"
+					+ " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+			.setParameter("orderIds", orderIds)
+			.getResultList();
+
+		Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+			.collect(Collectors.groupingBy(OrderItemQueryDto -> OrderItemQueryDto.getOrderId()));
+		return orderItemMap;
+	}
+
+	private static List<Long> toOrderIds(List<OrderQueryDto> result) {
+		List<Long> orderIds = result.stream()
+			.map(o -> o.getOrderId())
+			.collect(Collectors.toList());
+		return orderIds;
 	}
 
 	private List<OrderItemQueryDto> findOrderItems(Long orderId) {
