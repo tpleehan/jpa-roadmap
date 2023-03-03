f939945f79fac12e1f56e78454aef2f879f1bf81
tpleehan <tpleehan@gmail.com>
2023-03-04 03:43:36 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 - Spring Data JPA Simple introduce

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,36 +2,12 @@ package jpabook.jpashop.repository;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-	private final EntityManager em;
-
-	public void save(Member member) {
-		em.persist(member);
-	}
-
-	public Member findOne(Long id) {
-		return em.find(Member.class, id);
-	}
-
-	public List<Member> findAll() {
-		return em.createQuery("select m from Member m", Member.class)
-			.getResultList();
-	}
-
-	public List<Member> findByName(String name) {
-		return em.createQuery("select m from Member m where m.name = :name", Member.class)
-			.setParameter("name", name)
-			.getResultList();
-	}
-
+	// select m from Member m where m.name = ?
+	List<Member> findByName(String name);
 }
