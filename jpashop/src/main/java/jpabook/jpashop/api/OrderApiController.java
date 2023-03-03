f6e36a0717b07b17e3e374d559b77d1684b8934b
tpleehan <tpleehan@gmail.com>
2023-03-03 21:05:32 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 - API 개발 고급 - 컬렉션 조회 최적화(주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파)

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Address;
@@ -48,6 +49,19 @@ public class OrderApiController {
 	@GetMapping("/api/v3/orders")
 	public List<OrderDto> ordersV3() {
 		List<Order> orders = orderRepository.findAllWithItem();
+		List<OrderDto> result = orders.stream()
+			.map(o -> new OrderDto(o))
+			.collect(Collectors.toList());
+
+		return result;
+	}
+
+	@GetMapping("/api/v3.1/orders")
+	public List<OrderDto> ordersV3_page(
+		@RequestParam(value = "offset", defaultValue = "0") int offset,
+		@RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 		List<OrderDto> result = orders.stream()
 			.map(o -> new OrderDto(o))
 			.collect(Collectors.toList());
