ea7340ea1b9ae55c41a796849d598fe5911c003e
tpleehan <tpleehan@gmail.com>
2023-03-05 22:00:42 +0900

    실전! 스프링 데이터 JPA - 쿼리 메소드 기능(메소드 이름으로 쿼리 생성)

--- data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -59,7 +59,21 @@ public class MemberJpaRepositoryTest {
 
 		long deletedCount = memberJpaRepository.count();
 		assertThat(deletedCount).isEqualTo(0);
+	}
 
+	@Test
+	public void findByUsernameAndAgeGreaterThen() {
+		findByUsernameAndAgeGreaterThen
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("AAA", 20);
+		memberJpaRepository.save(m1);
+		memberJpaRepository.save(m2);
+
+		final List<Member> result = memberJpaRepository.findByUsernameAndAgeGreaterThen("AAA", 15);
+
+		assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+		assertThat(result.get(0).getAge()).isEqualTo(20);
+		assertThat(result.size()).isEqualTo(1);
 	}
 
 }
