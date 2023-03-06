4acac3f652e3827b94bc1985d47a3882c248716a
tpleehan <tpleehan@gmail.com>
2023-03-06 22:21:08 +0900

    실전! 스프링 데이터 JPA - 쿼리 메소드 기능(JPA Hint & Lock)

--- data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -247,4 +247,31 @@ class MemberRepositoryTest {
 		}
 	}
 
+	@Test
+	public void queryHint() {
+		// given
+		final Member member1 = new Member("member1", 10);
+		memberRepository.save(member1);
+		em.flush();
+		em.clear();
+
+		// when
+		Member findMember = memberRepository.findReadOnlyByUsername("member1");
+		findMember.setUsername("member2");
+
+		em.flush();
+	}
+
+	@Test
+	public void lock() {
+		// given
+		final Member member1 = new Member("member1", 10);
+		memberRepository.save(member1);
+		em.flush();
+		em.clear();
+
+		// when
+		final List<Member> result = memberRepository.findLockByUsername("member1");
+	}
+
 }
