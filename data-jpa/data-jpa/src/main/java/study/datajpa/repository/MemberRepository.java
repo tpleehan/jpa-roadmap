455aa42850f702908764bca8e3142a8764ec01ca
tpleehan <tpleehan@gmail.com>
2023-03-06 20:39:54 +0900

    실전! 스프링 데이터 JPA - 쿼리 메소드 기능(벌크성 수정 쿼리)

--- data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -38,4 +39,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findOptionalByUsername(String username); // 단건 Optional
 
 	Page<Member> findByAge(int age, Pageable pageable);
+
+	@Modifying(clearAutomatically = true)
+	@Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+	int bulkAgePlus(@Param("age") int age);
 }
--- data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +31,8 @@ class MemberRepositoryTest {
 	MemberRepository memberRepository;
 	@Autowired
 	TeamRepository teamRepository;
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	public void testMember() throws Exception {
@@ -190,6 +195,27 @@ class MemberRepositoryTest {
 		assertThat(page.getTotalPages()).isEqualTo(2);
 		assertThat(page.isFirst()).isTrue();
 		assertThat(page.hasNext()).isTrue();
+	}
+
+	@Test
+	public void bulkUpdate() {
+		// given
+		memberRepository.save(new Member("member1", 10));
+		memberRepository.save(new Member("member2", 19));
+		memberRepository.save(new Member("member3", 20));
+		memberRepository.save(new Member("member4", 21));
+		memberRepository.save(new Member("member5", 40));
+
+		// when
+		int resultCount = memberRepository.bulkAgePlus(20);
+		// em.clear(); @Modifying(clearAutomatically = true) clearAutomatically를 사용하면 clear()를 사용하지 않아도 된다.
+
+		List<Member> result = memberRepository.findByUsername("member5");
+		Member member5 = result.get(0);
+		System.out.println("member5 = " + member5);
+
+		// then
+		assertThat(resultCount).isEqualTo(3);
 	}
 
 }
