91860468db2555944661a520569e525e3d27ac28
tpleehan <tpleehan@gmail.com>
2023-03-06 12:25:51 +0900

    실전! 스프링 데이터 JPA - 쿼리 메소드 기능(@Query, 리포지토리 메소드에 쿼리 정의하기)

--- data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import study.datajpa.entity.Member;
@@ -11,7 +12,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
-	// @Query(name = "Member.findByUsername")
-	// 관례: Member 엔티티에 있는 NamedQuery를 찾기 때문에 메서드명과 일치하면 따로 Query를 사용하지 않아도 된다.
 	List<Member> findByUsername(@Param("username") String username);
+
+	@Query("select m from Member m where m.username = :username and m.age = :age")
+	List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
