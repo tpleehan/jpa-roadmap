cf2d91b557d54cd03c75ab76d3c170d490130061
tpleehan <tpleehan@gmail.com>
2023-03-07 17:16:24 +0900

    실전! 스프링 데이터 JPA - 나머지 기능들(Specifications (명세))

---
+++ data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberSpec.java
@@ -0,0 +1,31 @@
+package study.datajpa.repository;
+
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+
+public class MemberSpec {
+
+	public static Specification<Member> teamName(final String teamName) {
+		return (Specification<Member>)(root, query, builder) -> {
+
+			if (StringUtils.isEmpty(teamName)) {
+				return null;
+			}
+
+			Join<Member, Team> t = root.join("team", JoinType.INNER); // 회원과 조인
+			return builder.equal(t.get("name"), teamName);
+		};
+	}
+
+	public static Specification<Member> userName(final String username) {
+		return (Specification<Member>)(root, query, builder) ->
+			builder.equal(root.get("username"), username);
+	}
+
+}
