a3ac46f6bb17d9cb2dd11e4e5296d566ee0fa531
tpleehan <tpleehan@gmail.com>
2023-03-08 07:29:30 +0900

    실전! Querydsl - 프로젝트 환경설정(Querydsl 설정과 검증)

--- querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,43 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	// 자바 표준 spec에서는 @PersistenceContext를 사용한다.
+	// @PersistenceContext
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+			.selectFrom(qHello)
+			.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
