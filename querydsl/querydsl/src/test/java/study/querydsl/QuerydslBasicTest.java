91394997e4f2d6f9cf0cf7e7d79773e5f63d6225
tpleehan <tpleehan@gmail.com>
2023-03-08 21:58:16 +0900

    실전! Querydsl - 기본 문법(결과 조회)

--- querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,8 @@ package study.querydsl;
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.*;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import study.querydsl.entity.Member;
@@ -89,6 +92,32 @@ public class QuerydslBasicTest {
 			.fetchOne();
 
 		assertThat(findMember.getUsername()).isEqualTo("member1");
+	}
+
+	@Test
+	public void resultFetch() {
+		List<Member> fetch = queryFactory
+			.selectFrom(member)
+			.fetch();
+
+		Member fetchOne = queryFactory
+			.selectFrom(member)
+			.fetchOne();
+
+		Member fetchFirst = queryFactory
+			.selectFrom(member)
+			.fetchFirst();
+
+		QueryResults<Member> results = queryFactory
+			.selectFrom(member)
+			.fetchResults();
+
+		results.getTotal();
+		List<Member> content = results.getResults();
+
+		long total = queryFactory
+			.selectFrom(member)
+			.fetchCount();
 	}
 
 }
