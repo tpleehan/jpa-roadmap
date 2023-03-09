78e868a3b282b6754daee55209104ef679d039ee
tpleehan <tpleehan@gmail.com>
2023-03-09 14:47:08 +0900

    실전! Querydsl - 중급 문법(수정, 삭제 벌크 연산)

--- querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -627,4 +627,40 @@ public class QuerydslBasicTest {
 		return usernameEq(usernameCond).and(ageEq(ageCond));
 	}
 
+	@Test
+	public void bulkUpdate() {
+		long count = queryFactory
+			.update(member)
+			.set(member.username, "비회원")
+			.where(member.age.lt(28))
+			.execute();
+
+		em.flush();
+		em.clear();
+
+		List<Member> result = queryFactory
+			.selectFrom(member)
+			.fetch();
+
+		for (Member member1 : result) {
+			System.out.println("member1 = " + member1);
+		}
+	}
+
+	@Test
+	public void bulkAdd() {
+		long count = queryFactory
+			.update(member)
+			.set(member.age, member.age.add(2))
+			.execute();
+	}
+
+	@Test
+	public void bulkDelete() {
+		long count = queryFactory
+			.delete(member)
+			.where(member.age.gt(18))
+			.execute();
+	}
+
 }
